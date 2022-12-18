26dc47b5b82964e9374fc4f336addaaa68604047
psyxusheng <564764396@qq.com>
2022-12-18 09:40:34 +0800

    dataprocessor/vocab

--- dataprocessor.py
+++ dataprocessor.py
@@ -1,4 +1,6 @@
 import random 
+from torch.nn.utils.rnn import pad_sequence
+from torch import LongTensor , FloatTensor
 
 def padding(sequences,padding_idx = 0 , max_len = None):
     lengths = [len(seq) for seq in sequences]
@@ -18,25 +20,30 @@ def padding(sequences,padding_idx = 0 , max_len = None):
 
 class DataProcessor():
 
-    def __init__(self,sequences , targets ):
-
+    def __init__(self,vocab,sequences , targets ):
+        
+        self.vocab = vocab
         self.sequences , self.targets = sequences,targets 
         self.N = len(sequences)
+
+    def convert(self,sequence):
+        return LongTensor(self.vocab(sequence))
     
     def sample(self,batch_size):
         X,Y = [],[]
         for i in range(batch_size):
             index = random.randint(0,self.N-1)
             seq,targ = self.sequences[index] , self.targets[index]
-            X.append(seq)
+            X.append(self.convert(seq))
             Y.append(targ)
+        X = pad_sequence(X,padding_value=0,batch_first=True,)
+        Y = FloatTensor(Y)
         return X,Y
 
 if __name__ == '__main__':
-    dp = DataProcessor(['aa','bb','cc','aaa','bbb','ccc'],[1,2,3,1,2,3])
-    #x,y =  dp.sample(3)
-    #print(x,y)
-
-    seqs = ['123','1234','12345']
-    out = padding(seqs)
-    print(out)
+    from vocab import Vocab 
+    vocab = Vocab(['a','b','c'])
+    dp = DataProcessor(vocab,['aa','bb','cc','aaa','bbb','ccc'],[1,2,3,1,2,3])
+    x,y =  dp.sample(3)
+    print(x)
+    print(y)
--- vocab.py
+++ vocab.py
@@ -3,17 +3,37 @@ import os
 path = os.path.dirname(os.path.abspath(__file__))
 
 class Vocab():
-    def __init__(self,vocab_file = None):
+    
+    def __init__(self,vocab_list = []):
+        self.load(vocab_list)
+
+    def from_file(self, vocab_file = None):
+
         if vocab_file is None:
             vocab_file = os.path.join(path,'vocab.dat')
-        vocab = open(vocab_file,'r').read().split()
-        self._word_to_id = {word:i+1 for i,word in enumerate(vocab)}
-        self._id_to_word = {i+1:word for i,word in enumerate(vocab)}
+        vocab_list = open(vocab_file,'r').read().split()
+        self.loa(vocab_list)
+
+
+    def load(self,vocab_list):
+
+        self._word_to_id = {word:i+1 for i,word in enumerate(vocab_list)}
+        self._id_to_word = {i+1:word for i,word in enumerate(vocab_list)}
+        self._word_to_id['[pad]'] = 0
+        self._id_to_word[0] = '[pad]'
+
+
+    
     def convert(self,sequence):
         return [self._word_to_id.get(s,0) for s in sequence]
+    
     def convert_back(self,sequence):
         return [self._id_to_word.get(s,'unk') for s in sequence]
 
+    def __call__(self, sequence):
+        return self.convert(sequence)
+
 if __name__ == '__main__':
-    vocab = Vocab()
+    vocab_list = ['a','b','c','d','e']
+    vocab = Vocab(vocab_list)
     print(vocab._id_to_word)
